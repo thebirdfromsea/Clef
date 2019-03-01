18d9033e0a7c1c068b7ad5d7920a47b498f5d1c9
lkent4 <37677760+lkent4@users.noreply.github.com>
2019-02-28 20:17:56 -0600

    fix Tim's buttonTest.js
    Accidently overrided his updated code in last commit

--- src/Components/buttontest.js
+++ src/Components/buttontest.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
++import React, { Component } from 'react';
 
 export default class ButtonTest extends Component {
 
     constructor() {
         super();
         this.state = {
+            text: "Answer: "
         };
     }
 
-    clicked(text) {
+    clicked() {
         this.setState({ text: this.refs.textBox.value });
     }
 
     render() {
 
-        return <div>
-            <h2>1. Who is your favorite song artist?</h2>
-            <input ref="textBox" type="text" />
+        const questionStyle = { color: 'white', 'font-size': '30px', padding: "10px 20px", textAlign: "center" }
+
+        return <div style={questionStyle}>
+            <h2 style={questionStyle}>1. Who is your favorite song artist?</h2>
+            {this.state.text}<input ref="textBox" type="text" />
             <button onClick={(e) => { this.clicked(); }}>Submit</button>
-            <h2>2. What is your favorite genre of music?</h2>
-            <input ref="textBox" type="text" />
+            <h2 style={questionStyle}>2. What is your favorite genre of music?</h2>
+            {this.state.text}<input ref="textBox" type="text" />
             <button onClick={(e) => { this.clicked(); }}>Submit</button>
-            <h2>3. Are you in the dancing kind of mood?</h2>
-            <input ref="textBox" type="text" />
+            <h2 style={questionStyle}>3. Are you in the dancing kind of mood?</h2>
+            {this.state.text}<input ref="textBox" type="text" />
             <button onClick={(e) => { this.clicked(); }}>Submit</button>
         </div>;
     }
