baec219e6257c24a0082cde65c3b126c8eba98ab
David Zazulak <dzazul1@lsu.edu>
2019-03-07 17:00:09 -0600

    Convert the Song Search section from HTML to JSX

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Title from './title';
 import Playbutton from './playbutton';
 import Pausebutton from './pausebutton';
+import Songsearch from './songsearch';
 
 export default class Main extends Component {
     constructor() {
@@ -15,6 +16,7 @@ export default class Main extends Component {
         return (
             <div className="App">
              <Title />
+             <Songsearch />
              <Playbutton />
              <Pausebutton />
             </div>
---
+++ src/Components/songsearch.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+export default class Songsearch extends Component {
+    constructor() {
+    super();
+}
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+        <div className="row mt-10">
+        <div className="col" id="songSearch">
+        <h4 align="left">
+            Songs search
+            </h4>
+            <p align="left">
+                This section of the page is for the song search.
+                </p>
+                </div>
+                </div>
+                );
+            }
+        }
