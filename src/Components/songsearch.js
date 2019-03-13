1df45079681604fdfe1285599af0e38f8e300f4f
cmiddlebrook99 <46695600+cmiddlebrook99@users.noreply.github.com>
2019-03-12 22:52:51 -0500

    add song search feature

--- src/Components/songsearch.js
+++ src/Components/songsearch.js
@@ -1,69 +1,100 @@
 import React, { Component } from 'react';
 import { Search } from 'react-spotify-api';
+import { Form, Text, TextArea, Radio, RadioGroup, Select, Checkbox } from 'react-form';
 import SearchBar from 'material-ui-search-bar';
+import { Artist } from 'react-spotify-api'
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-const formatSearch = {
-    fontSize: "30px",
-    textAlign: "left",
-    padding: "0",
-    marginTop: "0",
-    marginLeft: "150px", 
-    color: "orange",
-}
 
-const searchSection = {
-    fontSize: "15px",
-    textAlign: "left",
-    margin: "100px",
-    color: "orange",
-}
+
 
 export default class Songsearch extends Component {
-    constructor() {
-        super();
-    }
-    componentDidMount() {
 
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            value : '  ',
+            display : 'defaultDisplay'
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
+  
+ 
+    handleChange(event) {
+        event.preventDefault() ;
+        this.setState({value: event.target.value,
+                       display: 'defaultDisplay'
+        }); 
+        console.log(this.state.value)
+      }
 
+    handleSubmit(event) {
+        event.preventDefault(); 
+        this.setState({display : 'loadDisplay'}) ; 
+        console.log(this.state.display) ;
+    }
     
-
+ 
     render() {
         return (
+            this.state.display == 'defaultDisplay'? (
+            <div> 
+           <form onSubmit={this.handleSubmit}>
+                <label>
+                Name:
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+           </form>
+           
+            </div>
+         ) : 
 
-            <div></div>
-        /*<div className="col" id="songSearch">
-
-                <SearchBar
-                    onChange={(newValue) => this.setState({value: newValue})}
-                    onRequestSearch={() => console.log('onChange')}
-                    style={{
-                    margin: '0 auto',
-                    maxWidth: 800
-                    }}
-                />
-
-                        <Search query={this.state.value} artist>
-                            {(data, loading, error) =>
-                                data ? (
-                                    <div>
-                                    <li>Artists</li>
-                                    <ul>
-                                        {data.artists.items.map(artist => (
-                                            <li key={artist.id}>{artist.name}</li>
-                                        ))}
-                                        </ul>
-                                        </div>
-                                        ): null
-
-                            }           s
-                        </Search>
-                    
-                }
-                  
-        </div>*/
+         (
+        <div> 
+         <form onSubmit={this.handleSubmit}>
+              <label>
+              Name:
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+              </label>
+              <input type="submit" value="Submit" />
+         </form>
+         <Search query= {this.state.value} album artist>
             
-        <div className="col" id="songSearch">
+            {(data, loading, error) =>
+                 data ? (
+                <ul>
+                    <li>Albums</li>
+                    <li>{this.state.value}</li>
+                    <ul>
+                        {data.albums.items.map(album => (
+                            <li key={album.id}>{album.name}</li>
+                        ))}
+                    </ul>
+                    <li>Artists</li>
+                    <ul>
+                        {data.artists.items.map(artist => (
+                            <li key={artist.id}>{artist.name}</li>
+                        ))}
+                    </ul>
+                </ul>
+            ) : null
+                }
+            </Search> 
+
+         
+            </div>)
+         
+            
+
+
+        ); 
+
+            
+   /*     <div className="col" id="songSearch">
         <h4 style={formatSearch}>
             Songs search
             </h4>
@@ -71,6 +102,7 @@ export default class Songsearch extends Component {
                 This section of the page is for the song search.
                 </p>
                 </div>
-                );
             }
+    */
         }
+ }
