e9692b10fdb4d51655e18db4781d280d7e2531d6
lkent4 <37677760+lkent4@users.noreply.github.com>
2019-03-26 22:04:22 -0500

    fix error with npm start
    was using old proxy configuration settings
    attempted to set up an equivalent proxy settings with new method but it did not work
    Going to have to go to grant/do more research to stop from losing the token

---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+/*const proxy = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(proxy('/Home', {
+        target: 'localhost:8888/login',
+        headers: {
+            "Connection": "keep-alive"
+        },
+    }));
+};*/
