b667e0544f4cf80e61d21d110c7c2429401e3f94
thebirdfromsea <46797898+thebirdfromsea@users.noreply.github.com>
2019-04-09 11:10:36 -0500

    Change pics in slideshow

--- src/Components/slidesshow.js
+++ src/Components/slidesshow.js
@@ -14,11 +14,11 @@ const style={
 const CarouselPage = () => {
   return (
     <MDBContainer>
-      <MDBCarousel activeItem={1} length={4} showControls={true} showIndicators={true} className="z-depth-1" data-interval=" 34" class="carousel slide carousel-fade"  style ={style}>
+      <MDBCarousel activeItem={1} length={4} showControls={false} showIndicators={false} className="z-depth-1" data-interval=" 34" class="carousel slide carousel-fade"  style ={style}>
         <MDBCarouselInner>
           <MDBCarouselItem itemId="1">
             <MDBView>
-              <img className="d-block w-100" src="http://drive.google.com/uc?export=view&id=1vqczlYYgnq-79ZT-e8cYFTTgGIx6kBnR"  alt="First slide"/>
+              <img className="d-block w-100" src="http://drive.google.com/uc?export=view&id=13KX3lZejvP9ClHoSRnm2Z3vYOoQW2OBS"  alt="First slide"/>
               {/* 16o2Uo2j7HAKw74dzwrwiW48HZCdwOC8y */}
               <MDBMask overlay="black-light" />
             </MDBView>
@@ -27,7 +27,7 @@ const CarouselPage = () => {
           </MDBCarouselItem>
           <MDBCarouselItem itemId="2">
             <MDBView>
-              <img className="d-block w-100" src="http://drive.google.com/uc?export=view&id=1ac92gSUW60d2naGgzTiqL3_3QedM_MCG" alt="Second slide" />
+              <img className="d-block w-100" src="http://drive.google.com/uc?export=view&id=1oTTqQL4Oy-5GqE4AMumYVfltK4v35yhd" alt="Second slide" />
               <MDBMask overlay="black-light" />
               {/* 1ep-KPOhobnZJT_WdjP4GHUG5ZdXekkH3 */}
             </MDBView>
@@ -36,7 +36,7 @@ const CarouselPage = () => {
           </MDBCarouselItem>
           <MDBCarouselItem itemId="3">
             <MDBView>
-              <img className="d-block w-100" src="http://drive.google.com/uc?export=view&id=1StmijkXBO25RNrZ6fF7ghpvZNBzg-s5z" alt="Third slide" />
+              <img className="d-block w-100" src="http://drive.google.com/uc?export=view&id=1nVCWHIo357gevZ1kZfjxostN5NYw0Bl6" alt="Third slide" />
               <MDBMask overlay="black-slight" />
             </MDBView>
             <MDBCarouselCaption>
@@ -44,7 +44,7 @@ const CarouselPage = () => {
           </MDBCarouselItem>
           <MDBCarouselItem itemId="4">
             <MDBView>
-              <img className="d-block w-100" src="http://drive.google.com/uc?export=view&id=1W0jlVMnOADUu05YLZZKMjKW5713QFVp9" alt="Mattonit's item" />
+              <img className="d-block w-100" src="http://drive.google.com/uc?export=view&id=1qKhmKwPycnsAyU7V3fc2BYHiAP_bMT24" alt="Mattonit's item" />
               <MDBMask overlay="black-light" />
             </MDBView>
             <MDBCarouselCaption>
